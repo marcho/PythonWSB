69f6c029f82a32963ad191282a306544127050cb
marcho <tomczak.luk@gmail.com>
2018-09-30 13:56:50 +0200

    liczenie

---
+++ liczenie.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+print("Liczenie od 0 do 10")
+for i in range(11):
+	print (i)
+	
+print("Liczenie od 0 do 100 co 5")
+for z in range(0, 101, 5):
+	print(z)
+	
+print("Liczenie od 20 do 10")
+for x in range(20, 9, -1):
+	print(x)
