263ad61cad6a28d390f8c9e5f3f859990fbc8c38
marcho <tomczak.luk@gmail.com>
2018-09-30 13:44:35 +0200

    kosci

---
+++ kosci.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+
+import random
+koscPierwsza = random.randint(1, 6)
+koscDruga = random.randint(1, 6)
+sumaKosci = koscPierwsza + koscDruga
+print("Suma wyrzuconych kości to: ", sumaKosci)
